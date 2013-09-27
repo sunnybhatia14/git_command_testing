e7765b4343a4a9cd8ee030ae01f009a7d81afd1c
unknown <sunny.bhatia@MPST-L030.MPSTECHNOLOGIES.COM>
2013-09-27 15:52:57 +0530

    My first test checkin

---
+++ Test1.java
@@ -0,0 +1,19 @@
+package webDriver_Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test1 {
+
+	public static void main(String args[]) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("cheeese!");
+		element.submit();
+		System.out.println("page title is " + driver.getTitle());
+	}
+}
+
